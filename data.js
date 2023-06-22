99cc0652af3e31a6bac5262af5717afa71e95d3a
LucaBla <me@lucablazevic.dev>
2023-06-22 16:45:55 +0200

    add show

--- data.js
+++ data.js
@@ -3,7 +3,10 @@ export const terms = [
     title: 'Absatzlogistik', 
     sections:[
       {title: 'Überblick', 
-      description: 'Der Begriff der Absatzlogistik (auch: Distributionslogistik) bezeichnet alle Prozesse der Distributionspolitik, die sicherstellt, dass Güter vom Unternehmen an den jeweiligen Kunden gelangen. \n\nIm Fokus stehen hierbei sämtliche Abläufe, die zur Sicherstellung der entsprechenden Marktversorgung notwendig sind. Die Absatzlogistik ist organisatorisch verortet zwischen dem Kunden, der ein Produkt nachfragt, sowie der vorgelagerten Produktionslogistik.'
+      description: 'Der Begriff der Absatzlogistik (auch: Distributionslogistik) bezeichnet alle Prozesse der Distributionspolitik, die sicherstellt, dass Güter vom Unternehmen an den jeweiligen <a href="https://wirtschaftslexikon.gabler.de/definition/kunde-37108" target="_blank">Kunden</a> gelangen. \n\nIm Fokus stehen hierbei sämtliche Abläufe, die zur Sicherstellung der entsprechenden Marktversorgung notwendig sind. Die Absatzlogistik ist organisatorisch verortet zwischen dem <a href="https://wirtschaftslexikon.gabler.de/definition/kunde-37108" target="_blank">Kunden</a>, der ein Produkt nachfragt, sowie der vorgelagerten <a href="show.html?id=4">Produktionslogistik</a>.'
+      },
+      {title: 'Aufgaben', 
+      description: 'Zur wesentlichen Aufgabe und Funktion der Absatzlogistik zählen Planung, Ausführung und Überwachung. Betrachtet werden dabei alle <a href="show.html?id=5">Materialflüsse</a> sowie Informationsflüsse, die notwendig sind, um Güter zur gewünschten Zeit, in der gewünschten Menge und <a href="https://wirtschaftslexikon.gabler.de/definition/qualitaet-45908" target="_blank">Qualität</a> an den jeweiligen Bestimmungsort zu bringen.'
       }
     ] 
   },
@@ -20,15 +23,21 @@ export const terms = [
     }
   ] },
   { id: 4,
-    title: 'Incoterms', sections:[
+    title: 'Produktionslogistik', sections:[
     {title: 'Überblick', 
-    description: 'Der Begriff der Absatzlogistik (auch: Distributionslogistik) bezeichnet alle Prozesse der Distributionspolitik, die sicherstellt, dass Güter vom Unternehmen an den jeweiligen Kunden gelangen. \n\nIm Fokus stehen hierbei sämtliche Abläufe, die zur Sicherstellung der entsprechenden Marktversorgung notwendig sind. Die Absatzlogistik ist organisatorisch verortet zwischen dem Kunden, der ein Produkt nachfragt, sowie der vorgelagerten Produktionslogistik.'
+    description: 'Die Produktionslogistik bezieht sich auf die Planung, Organisation und Steuerung von <a href="show.html?id=5">Materialflüssen</a> und logistischen Prozessen innerhalb der Produktion eines Unternehmens. \n\nSie umfasst alle Aktivitäten, die darauf abzielen, die Materialversorgung, den Produktionsablauf und die Warenverteilung zu optimieren, um eine effiziente und reibungslose Produktion zu gewährleisten. \n\nGleichzeitig ist die Produktionslogistik eng mit der <a href="show.html?id=1">Absatzlogistik</a> verbunden. Eine enge Abstimmung zwischen beiden Bereichen ist entscheidend, um eine reibungslose <a href="https://wirtschaftslexikon.gabler.de/definition/supply-chain-management-scm-49361" target="_blank">Lieferkette</a> von der Produktion bis zur Auslieferung zu gewährleisten und die Kundenzufriedenheit zu maximieren.'
+    },
+    {title: 'Aufgaben', 
+    description: 'Die Produktionslogistik umfasst die Aufgaben der Materialwirtschaft, Produktionsplanung, Arbeitsvorbereitung, Produktionssteuerung, Qualitätskontrolle sowie Versand und Distribution. \n\nSie zielt darauf ab, einen reibungslosen Produktionsablauf zu gewährleisten, Lagerbestände zu optimieren und die Kundenzufriedenheit zu steigern. Modernste Technologien unterstützen dabei, die Produktionslogistik effizienter zu gestalten.'
     }
   ] },
   { id: 5,
-    title: 'Last-Mile-Delivery', sections:[
+    title: 'Materialfluss', sections:[
     {title: 'Überblick', 
-    description: 'Der Begriff der Absatzlogistik (auch: Distributionslogistik) bezeichnet alle Prozesse der Distributionspolitik, die sicherstellt, dass Güter vom Unternehmen an den jeweiligen Kunden gelangen. \n\nIm Fokus stehen hierbei sämtliche Abläufe, die zur Sicherstellung der entsprechenden Marktversorgung notwendig sind. Die Absatzlogistik ist organisatorisch verortet zwischen dem Kunden, der ein Produkt nachfragt, sowie der vorgelagerten Produktionslogistik.'
+    description: 'Der Materialfluss bezieht sich auf den Weg, den Materialien oder Güter innerhalb eines Unternehmens oder einer Organisation nehmen, angefangen von der Beschaffung über die Produktion bis hin zur Auslieferung an den <a href="https://wirtschaftslexikon.gabler.de/definition/kunde-37108" target="_blank">Kunden</a>. Er umfasst alle logistischen Aktivitäten und Prozesse, die erforderlich sind, um den reibungslosen Transport, die Lagerung und den Umgang mit Materialien zu gewährleisten. \n\nDer Materialfluss ist entscheidend für eine effiziente <a href="show.html?id=4">Produktions</a>- und <a href="show.html?id=1">Distributionslogistik</a>. Durch eine optimale Gestaltung des Materialflusses können Engpässe und Verschwendung reduziert werden. Es geht darum, die richtigen Materialien zur richtigen Zeit am richtigen Ort verfügbar zu haben, um den Produktionsablauf nicht zu unterbrechen und Liefertermine einhalten zu können.'
+    },
+    {title: 'Aspekte', 
+    description: 'Der Materialfluss umfasst verschiedene Aspekte wie den Transport von Rohstoffen und Zwischenprodukten, die Lagerung und Kommissionierung von Materialien, den innerbetrieblichen Transport von Waren sowie den Versand und die Distribution der fertigen Produkte. Dabei spielt die Planung, Steuerung und Überwachung des Materialflusses eine entscheidende Rolle, um die Prozesse effizient zu gestalten und Kosten zu minimieren.'
     }
   ] },
 ];
--- show.js
+++ show.js
@@ -4,6 +4,30 @@ const urlParams = new URLSearchParams(window.location.search);
 
 document.addEventListener('DOMContentLoaded', loadContent);
 
-function loadContent(){
-  console.log(terms.find((term) => term.id === parseInt(urlParams.get('id'))));
+function loadContent() {
+  const term = terms.find((term) => term.id === parseInt(urlParams.get('id')));
+
+  const header = document.getElementById('show-header');
+  const headerText = document.createElement('h1');
+
+  headerText.textContent = term.title;
+
+  header.appendChild(headerText);
+
+  const sectionsContainer = document.getElementById('show-sections-container');
+
+  term.sections.forEach((data) => {
+    const section = document.createElement('section');
+
+    const sectionHeader = document.createElement('h2');
+    const sectionContent = document.createElement('div');
+
+    sectionHeader.textContent = data.title;
+    sectionContent.innerHTML = data.description;
+
+    section.appendChild(sectionHeader);
+    section.appendChild(sectionContent);
+    sectionsContainer.appendChild(section);
+  });
+
 }
