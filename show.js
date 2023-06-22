a8e956a96447d2fa2e05febaf26302a1db99727f
LucaBla <me@lucablazevic.dev>
2023-06-22 17:02:41 +0200

    add lastEdited to data.js

--- show.js
+++ show.js
@@ -7,27 +7,49 @@ document.addEventListener('DOMContentLoaded', loadContent);
 function loadContent() {
   const term = terms.find((term) => term.id === parseInt(urlParams.get('id')));
 
+  addHeader(term.title)
+
+  addSections(term);
+
+}
+
+function addHeader(title){
   const header = document.getElementById('show-header');
   const headerText = document.createElement('h1');
 
-  headerText.textContent = term.title;
+  headerText.textContent = title;
 
   header.appendChild(headerText);
+}
 
-  const sectionsContainer = document.getElementById('show-sections-container');
-
+function addSections(term){
   term.sections.forEach((data) => {
-    const section = document.createElement('section');
 
-    const sectionHeader = document.createElement('h2');
-    const sectionContent = document.createElement('div');
+    addSection(data.title, data.description);
 
-    sectionHeader.textContent = data.title;
-    sectionContent.innerHTML = data.description;
-
-    section.appendChild(sectionHeader);
-    section.appendChild(sectionContent);
-    sectionsContainer.appendChild(section);
   });
+}
 
+function addSection(title, content){
+  const sectionsContainer = document.getElementById('show-sections-container');
+  const section = document.createElement('section');
+
+  addSectionHeader(section, title);
+  addSectionContent(section, content);
+
+  sectionsContainer.appendChild(section);
+}
+
+function addSectionHeader(section, title){
+  const sectionHeader = document.createElement('h2');
+
+  sectionHeader.textContent = title;
+
+  section.appendChild(sectionHeader);
+}
+
+function addSectionContent(section, content){
+  const sectionContent = document.createElement('div');
+  sectionContent.innerHTML = content;
+  section.appendChild(sectionContent);
 }
